63197dd987349d81e57e088050d2c922d36d7871
ameenah <ameenah.k.a.m@gmail.com>
2018-09-17 12:23:07 +0200

    /ReplaceFile function

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ func main() {
 	http.HandleFunc("/GetFile", GetFile)
 	http.HandleFunc("/ModifyFile", modifyFile)
 	http.HandleFunc("/GetLogTail", getLogTail)
+	http.HandleFunc("/ReplaceFile", replaceFile)
 
 	// Binary File upload/download
 	http.HandleFunc("/ReceiveFile", receiveFile)
